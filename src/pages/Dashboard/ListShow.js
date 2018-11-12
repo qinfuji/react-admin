33206615631bb17d1a2efb3df82865f9cf81431a
qinfj <qinfj@ifeng.com>
2018-11-12 12:51:55 +0800

    first commit

---
+++ src/pages/Dashboard/ListShow.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class ListShow extends Component {
+  render() {
+    return 'ListShow';
+  }
+}
+
+export default ListShow;
---
+++ src/pages/Dashboard/MapShow.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class MapShow extends Component {
+  render() {
+    return 'MapShow';
+  }
+}
+
+export default MapShow;
